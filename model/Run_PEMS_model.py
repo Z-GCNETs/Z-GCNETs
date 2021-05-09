83ce583884053d75fee5609bb0b1ee28edbd1953
Yuzhou Chen <yuzhouchen888@gmail.com>
2021-05-08 23:58:15 -0500

    Z-GCNETs

---
+++ model/Run_PEMS_model.py
@@ -0,0 +1,155 @@
+import os
+import sys
+file_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(file_dir)
+import torch
+import torch.nn as nn
+import argparse
+import configparser
+from datetime import datetime
+from ZGCNETs import NNLSGCRNCNN as Network
+from Trainer import Trainer
+from TrainInits import init_seed
+from dataloader import get_dataloader, get_topo_dataloader
+from TrainInits import print_model_parameters
+from metrics import MAE_torch
+
+#-----------------------------------------------------------------------------#
+# dataset and
+Mode = 'train'
+DEBUG = 'True'
+DATASET = 'PEMSD4' #PEMSD4 or PEMSD8
+DEVICE = 'cuda:0'
+MODEL = 'ZGCNETs'
+H_type = 'H1' #dimension of zigzag PI
+data_type = '4' # 4 or 8 for PEMSDX dataset
+
+#get configuration
+config_file = '{}_{}.conf'.format(DATASET, MODEL)
+config = configparser.ConfigParser()
+config.read(config_file)
+print("++++++++++++++")
+print(config_file) # using PEMSD4_AGCRN.conf as "config"!
+print("++++++++++++++")
+#-----------------------------------------------------------------------------#
+
+def masked_mae_loss(scaler, mask_value):
+    def loss(preds, labels):
+        if scaler:
+            preds = scaler.inverse_transform(preds)
+            labels = scaler.inverse_transform(labels)
+        mae = MAE_torch(pred=preds, true=labels, mask_value=mask_value)
+        return mae
+    return loss
+
+#parser
+args = argparse.ArgumentParser(description='arguments')
+args.add_argument('--dataset', default=DATASET, type=str)
+args.add_argument('--mode', default=Mode, type=str)
+args.add_argument('--device', default=DEVICE, type=str, help='indices of GPUs')
+args.add_argument('--debug', default=DEBUG, type=eval)
+args.add_argument('--model', default=MODEL, type=str)
+args.add_argument('--cuda', default=True, type=bool)
+# from data, these below information could be found in .conf file
+#data
+args.add_argument('--val_ratio', default=config['data']['val_ratio'], type=float)
+args.add_argument('--test_ratio', default=config['data']['test_ratio'], type=float)
+args.add_argument('--lag', default=config['data']['lag'], type=int)
+args.add_argument('--horizon', default=config['data']['horizon'], type=int)
+args.add_argument('--num_nodes', default=config['data']['num_nodes'], type=int)
+args.add_argument('--tod', default=config['data']['tod'], type=eval)
+args.add_argument('--normalizer', default=config['data']['normalizer'], type=str)
+args.add_argument('--column_wise', default=config['data']['column_wise'], type=eval)
+args.add_argument('--default_graph', default=config['data']['default_graph'], type=eval)
+#model
+args.add_argument('--input_dim', default=config['model']['input_dim'], type=int)
+args.add_argument('--output_dim', default=config['model']['output_dim'], type=int)
+args.add_argument('--embed_dim', default=config['model']['embed_dim'], type=int)
+args.add_argument('--rnn_units', default=config['model']['rnn_units'], type=int)
+args.add_argument('--num_layers', default=config['model']['num_layers'], type=int)
+args.add_argument('--link_len', default=config['model']['link_len'], type=int)
+args.add_argument('--gamma', default=config['model']['gamma'], type=float)
+args.add_argument('--window_len', default=config['model']['window_len'], type=int)
+#train
+args.add_argument('--loss_func', default=config['train']['loss_func'], type=str)
+args.add_argument('--seed', default=config['train']['seed'], type=int)
+args.add_argument('--batch_size', default=config['train']['batch_size'], type=int)
+args.add_argument('--epochs', default=config['train']['epochs'], type=int)
+args.add_argument('--lr_init', default=config['train']['lr_init'], type=float)
+args.add_argument('--lr_decay', default=config['train']['lr_decay'], type=eval)
+args.add_argument('--lr_decay_rate', default=config['train']['lr_decay_rate'], type=float)
+args.add_argument('--lr_decay_step', default=config['train']['lr_decay_step'], type=str)
+args.add_argument('--early_stop', default=config['train']['early_stop'], type=eval)
+args.add_argument('--early_stop_patience', default=config['train']['early_stop_patience'], type=int)
+args.add_argument('--grad_norm', default=config['train']['grad_norm'], type=eval)
+args.add_argument('--max_grad_norm', default=config['train']['max_grad_norm'], type=int)
+args.add_argument('--teacher_forcing', default=False, type=bool)
+args.add_argument('--real_value', default=config['train']['real_value'], type=eval, help = 'use real value for loss calculation')
+#test
+args.add_argument('--mae_thresh', default=config['test']['mae_thresh'], type=eval)
+args.add_argument('--mape_thresh', default=config['test']['mape_thresh'], type=float)
+#log
+args.add_argument('--log_dir', default='./', type=str)
+args.add_argument('--log_step', default=config['log']['log_step'], type=int)
+args.add_argument('--plot', default=config['log']['plot'], type=eval)
+args = args.parse_args()
+init_seed(args.seed)
+if torch.cuda.is_available():
+    torch.cuda.set_device(int(args.device[5]))
+else:
+    args.device = 'cpu'
+
+#init model
+model = Network(args)
+model = model.to(args.device)
+for p in model.parameters():
+    if p.dim() > 1:
+        nn.init.xavier_uniform_(p)
+    else:
+        nn.init.uniform_(p)
+print_model_parameters(model, only_num=False)
+
+#load dataset
+train_loader, val_loader, test_loader, scaler = get_topo_dataloader(args, data_type, H_type,
+                                                               normalizer=args.normalizer,
+                                                               tod=args.tod, dow=False,
+                                                               weather=False, single=False)
+
+#init loss function, optimizer
+if args.loss_func == 'mask_mae':
+    loss = masked_mae_loss(scaler, mask_value=0.0)
+elif args.loss_func == 'mae':
+    loss = torch.nn.L1Loss().to(args.device)
+elif args.loss_func == 'mse':
+    loss = torch.nn.MSELoss().to(args.device)
+else:
+    raise ValueError
+
+optimizer = torch.optim.Adam(params=model.parameters(), lr=args.lr_init, eps=1.0e-8,
+                             weight_decay=0, amsgrad=False)
+#learning rate decay
+lr_scheduler = None
+if args.lr_decay:
+    print('Applying learning rate decay.')
+    lr_decay_steps = [int(i) for i in list(args.lr_decay_step.split(','))]
+    lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer,
+                                                        milestones=lr_decay_steps,
+                                                        gamma=args.lr_decay_rate)
+
+#config log path
+current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+current_dir = os.path.dirname(os.path.realpath(__file__))
+log_dir = os.path.join(current_dir,'experiments', args.dataset, current_time)
+args.log_dir = log_dir
+
+#start training
+trainer = Trainer(model, loss, optimizer, train_loader, val_loader, test_loader, scaler,
+                  args, lr_scheduler=lr_scheduler)
+if args.mode == 'train':
+    trainer.train()
+elif args.mode == 'test':
+    model.load_state_dict(torch.load('../pre-trained/{}.pth'.format(args.dataset)))
+    print("Load saved model")
+    trainer.test(model, trainer.args, test_loader, scaler, trainer.logger)
+else:
+    raise ValueError
